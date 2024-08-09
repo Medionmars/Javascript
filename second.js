1501436cf83f8a177f34e5eda67a6015e629bdac
Medi <136518976+Medionmars@users.noreply.github.com>
2024-08-09 18:54:38 +0200

    Second.js

---
+++ second.js
@@ -0,0 +1,11 @@
+var q = 2 + 5, w = 4-3, e = 8 * 3, r = 36 / 2;
+console.log(q, w, e, r);
+// This are just basic math commands you are also able to add two string togheter like dog + cat is catdog in 1 string
+
+var t = 20 % 2, y = 19 % 2;
+console.log(t, y);
+
+var u = 20 % 2 === 0;
+console.log(u);
+// Here you can check the remainers so the .0 after a number and with the === at the end you can ask 
+// if the remainers is for example you can do this with variables that are a int or float you could aslo do it with string
